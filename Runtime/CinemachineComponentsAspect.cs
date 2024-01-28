bcc7cc2280e7d5de216daf2be906aa7a02c2aa81
godzzz <g0dzZz1lla@yandex.ru>
2024-01-28 18:13:56 +0400

    [v0.0.1] First release

---
+++ Runtime/CinemachineComponentsAspect.cs
@@ -0,0 +1,18 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2024 Nikolay Melnikov <n.melnikov@depra.org>
+
+using Depra.Ecs.Worlds;
+
+namespace Depra.Ecs.Components.Cinemachine
+{
+	public sealed class CinemachineComponentsAspect : IComponentAspect
+	{
+		void IComponentAspect.Initialize(World world)
+		{
+			world.AddAspect(this);
+			world.AddPool(new ComponentPool<CinemachineVirtualCameraRef>());
+		}
+
+		void IComponentAspect.PostInitialize() { }
+	}
+}
