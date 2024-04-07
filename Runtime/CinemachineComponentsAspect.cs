57cbf51d762e9e67c165b409b4a6ac2c43ce8c0a
godzzz <g0dzZz1lla@yandex.ru>
2024-04-08 00:15:02 +0400

    [v0.0.3] Native dependency updated

--- Runtime/CinemachineComponentsAspect.cs
+++ Runtime/CinemachineComponentsAspect.cs
@@ -1,19 +1,15 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
 // © 2024 Nikolay Melnikov <n.melnikov@depra.org>
 
-using Depra.Ecs.Worlds;
-
 namespace Depra.Ecs.Components.Cinemachine
 {
 	public sealed class CinemachineComponentsAspect : IComponentAspect
 	{
-		void IComponentAspect.Initialize(World world)
+		void IComponentAspect.Initialize(AspectGroup aspects, ComponentPoolGroup pools)
 		{
-			world.AddAspect(this);
-			world.AddPool(new ComponentPool<CinemachineVirtualCameraRef>());
-			world.AddPool(new ComponentPool<CinemachineVirtualCameraBaseRef>());
+			aspects.Add(this);
+			pools.Add(new ComponentPool<CinemachineVirtualCameraRef>());
+			pools.Add(new ComponentPool<CinemachineVirtualCameraBaseRef>());
 		}
-
-		void IComponentAspect.PostInitialize() { }
 	}
 }
